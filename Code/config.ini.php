074cec4d3758e57ff217f35e0e6c9ba3c669567d
Achintha Gunasekara <achintha@me.com>
2015-11-10 09:17:40 +1100

    Update config.ini.php

--- Code/config.ini.php
+++ Code/config.ini.php
@@ -1,5 +1,10 @@
 <?php
 
+/* 
+ * Archie Gunasekara
+ * 2014
+ */
+
 //Update to match the location of the this application
 define("BASE_PATH", 'C:\Program Files (x86)\Apache Software Foundation\Apache2.2\htdocs\GoogleAuthAppPortal');
 
