d3607d94aeb507a42aabcd5a2c72e3d26a4a2759
Achintha Gunasekara <achintha@me.com>
2015-11-11 09:16:47 +1100

    Update extensions.php

--- Code/extensions.php
+++ Code/extensions.php
@@ -4,8 +4,10 @@
  * Archie Gunasekara
  * 2014
  */
-	print "<h1>PHP Loaded Extensions</h1>";
-	print "<pre>";
-	print_r(get_loaded_extensions());
-	print "</pre>";
+
+print "<h1>PHP Loaded Extensions</h1>";
+print "<pre>";
+print_r(get_loaded_extensions());
+print "</pre>";
+
 ?>
